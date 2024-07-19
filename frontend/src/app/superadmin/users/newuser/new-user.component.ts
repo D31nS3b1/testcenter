9d5aff491303e60eebd0282c2debd7f6ecbe0d8b
Sebastian Gerbeth <150120032+D31nS3b1@users.noreply.github.com>
2024-07-19 11:24:08 +0200

    Update new-user.component.ts
    Update Passwort-Richtlinien

--- frontend/src/app/superadmin/users/newuser/new-user.component.ts
+++ frontend/src/app/superadmin/users/newuser/new-user.component.ts
@@ -8,6 +8,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class NewUserComponent {
   newUserForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    pw: new FormControl('', [Validators.required, Validators.minLength(7)])
+    pw: new FormControl('', [Validators.required, Validators.minLength(7), Validators.pattern('(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\\W)')])
   });
 }
