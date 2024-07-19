ff8aa5124108d4fc8ec47e5ea19d7f4ea66a8c5f
Sebastian Gerbeth <150120032+D31nS3b1@users.noreply.github.com>
2024-07-19 12:12:06 +0200

    Update new-password.component.ts

--- frontend/src/app/superadmin/users/newpassword/new-password.component.ts
+++ frontend/src/app/superadmin/users/newpassword/new-password.component.ts
@@ -2,13 +2,15 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Component, Inject } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 
+const upperLowerSymbolNumberRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+
 @Component({
   templateUrl: './new-password.component.html'
 })
 
 export class NewPasswordComponent {
   newPasswordForm = new FormGroup({
-    pw: new FormControl('', [Validators.required, Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{10,60}$/)])
+    pw: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern(upperLowerSymbolNumberRegex)])
   });
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: string) { }
