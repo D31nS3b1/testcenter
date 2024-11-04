f8fe954e5cb8ef26ef25d23fb710ece210311a99
Sebastian Gerbeth <150120032+D31nS3b1@users.noreply.github.com>
2024-11-04 15:14:45 +0100

    Update new-user.component.ts
    Change Password-Policy to be at least 10 characters long and contain upper and lower case letters, numbers and symbols.

--- frontend/src/app/superadmin/users/newuser/new-user.component.ts
+++ frontend/src/app/superadmin/users/newuser/new-user.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+const UpperLowerSymbolNumber = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+
 @Component({
   templateUrl: './new-user.component.html'
 })
 
 export class NewUserComponent {
   newUserForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    pw: new FormControl('', [Validators.required, Validators.minLength(7)])
+    name: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern(UpperLowerSymbolNumber)]),
+    pw: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern(UpperLowerSymbolNumber)])
   });
 }
