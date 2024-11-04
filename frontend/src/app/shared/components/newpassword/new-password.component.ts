47fe821940343f453682f3208cd8d17d7bd5649c
Sebastian Gerbeth <150120032+D31nS3b1@users.noreply.github.com>
2024-11-04 08:30:38 +0100

    Update new-password.component.ts
    Change Password-Policy
    The password should be at least 10 characters long and contain upper and lower case letters, numbers and symbols.

--- frontend/src/app/shared/components/newpassword/new-password.component.ts
+++ frontend/src/app/shared/components/newpassword/new-password.component.ts
@@ -3,6 +3,8 @@ import { Component, Inject } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { samePasswordValidator } from '../../validators/samePassword.validator';
 
+const UpperLowerSymbolNumber = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+
 @Component({
   templateUrl: './new-password.component.html'
 })
@@ -10,7 +12,7 @@ import { samePasswordValidator } from '../../validators/samePassword.validator';
 export class NewPasswordComponent {
   newPasswordForm = new FormGroup({
     pw: new FormControl('', [Validators.required, Validators.minLength(7)]),
-    pw_confirm: new FormControl('', [Validators.required, Validators.minLength(7)])
+    pw_confirm: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern(UpperLowerSymbolNumber)])
   },
   { validators: samePasswordValidator }
   );
